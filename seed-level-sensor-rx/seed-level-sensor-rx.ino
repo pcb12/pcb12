57fe5ea2c777c60201b1285e826d349f1f9118d6
steven haule <143690068+pcb12@users.noreply.github.com>
2023-09-22 00:20:30 -0700

    Add files via upload
    code for iot nrf24lo1 for sending data

---
+++ seed-level-sensor-rx/seed-level-sensor-rx.ino
@@ -0,0 +1,36 @@
+// receiver code
+#define buzzer 10
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+
+const byte address[6] = "00001";
+
+void setup(){
+  Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
+
+}
+
+void loop(){
+  if(radio.available()){
+    int  distance;
+    radio.read(&distance, sizeof(distance));
+    Serial.println(distance);
+    delay(200);
+  }
+  if(distance<=4)
+  {
+    digitalWrite(buzzer,LOW)
+  }
+  else if(distance>=30)
+  {
+    digitalWrite(buzzer,HIGH);
+  }
+}
---
+++ seed_level_sensor-tx/seed_level_sensor-tx.ino
@@ -0,0 +1,47 @@
+// author qualchriss
+
+
+// transmitter code
+
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // CE, CSN
+const int trigPin =6;
+const int echoPin =5;
+long duration;
+int distance;
+const byte address[6] = "00001";
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
+}
+
+void loop() {
+  sensor();
+  radio.write(&distance, sizeof(distance));
+  delay(1000);
+}
+
+void sensor()
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distance= (duration/2)/29.1;
+Serial.print("Distance: ");
+Serial.println(distance);
+delay(300);
+}
+
+
